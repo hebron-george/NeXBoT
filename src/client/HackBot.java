655b04152623e12490a716f9c2a89f5f3523f687
ras1 <ras1@mail.usf.edu>
2013-07-06 22:10:25 -0400

    Externalized strings needed for login, updated readme.txt to reflect this.

--- src/client/HackBot.java
+++ src/client/HackBot.java
@@ -8,12 +8,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class HackBot implements Runnable {
-	private String server = "irc.quakenet.org";
-	private String nick = "botnick";
-	private String login = "botlogin";
-	private String cq2User = "";
-	private String cq2Pass = "";
-	private String channel = "#siralim";
+	private String server = stringAccessor.getString("HackBot.server"); //$NON-NLS-1$
+	private String nick = stringAccessor.getString("HackBot.nick"); //$NON-NLS-1$
+	private String login = stringAccessor.getString("HackBot.user"); //$NON-NLS-1$
+	private String cq2User = stringAccessor.getString("HackBot.cq2User"); //$NON-NLS-1$
+	private String cq2Pass = stringAccessor.getString("HackBot.cq2User"); //$NON-NLS-1$
+	private String channel = stringAccessor.getString("HackBot.channel"); //$NON-NLS-1$
 	
 	private Socket socket;
 	private BufferedWriter writer;
@@ -24,7 +24,7 @@ public class HackBot implements Runnable {
 	protected HackBot() {}
 	
 	public static void main(String[] args) throws Exception {
-        System.out.println("Starting program.");
+        System.out.println("Starting program."); //$NON-NLS-1$
 		try {
         	new HackBot().start();
         } catch (java.io.IOException e) {
@@ -33,45 +33,45 @@ public class HackBot implements Runnable {
 	}
 	
 	private void start() throws Exception {
-		System.out.println("Initializing.");
+		System.out.println("Initializing."); //$NON-NLS-1$
 		this.socket = new Socket(server, 6667);
 		this.logger = LogManager.getLogger(HackBot.class.getName());
         this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream( )));
         this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream( )));
 
         // Log on to the server.
-        logger.trace("Logging into IRC server.");
-        writer.write("NICK " + this.nick + "\r\n");
-        logger.trace("NICK " + this.nick + "\r\n");
-        writer.write("USER " + this.login + " 8 * : Java IRC Hacks Bot\r\n");
-        logger.trace("USER " + this.login + " 8 * : Java IRC Hacks Bot\r\n");
+        logger.trace("Logging into IRC server."); //$NON-NLS-1$
+        writer.write("NICK " + this.nick + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
+        logger.trace("NICK " + this.nick + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
+        writer.write("USER " + this.login + " 8 * : Java IRC Hacks Bot\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
+        logger.trace("USER " + this.login + " 8 * : Java IRC Hacks Bot\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
         writer.flush( );
         
         // Read lines from the server until it tells us we have connected.
         String line = null;
         while ((line = reader.readLine( )) != null) {
         	System.out.println(line);
-            if (line.indexOf("004") >= 0) {
+            if (line.indexOf("004") >= 0) { //$NON-NLS-1$
                 // We are now logged in.
                 break;
             }
-            else if (line.indexOf("433") >= 0) {
-                System.out.println("Nickname is already in use.");
+            else if (line.indexOf("433") >= 0) { //$NON-NLS-1$
+                System.out.println("Nickname is already in use."); //$NON-NLS-1$
                 return;
             }
 
-            if (line.startsWith("PING ")) {
+            if (line.startsWith("PING ")) { //$NON-NLS-1$
                 // We must respond to PINGs to avoid being disconnected.
-                writer.write("PONG " + line.substring(5) + "\r\n");
-                System.out.println("PONG " + line.substring(5) + "\r\n");
+                writer.write("PONG " + line.substring(5) + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
+                System.out.println("PONG " + line.substring(5) + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
                 writer.flush( );
             }
         }
         
-        logger.trace("Logged into server successfully.");
+        logger.trace("Logged into server successfully."); //$NON-NLS-1$
         
         // Join the channel.
-        writer.write("JOIN " + channel + "\r\n");
+        writer.write("JOIN " + channel + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
         writer.flush( );
         
         new Thread(this).start();
@@ -84,53 +84,53 @@ public class HackBot implements Runnable {
 	        	// Print the raw line received by the bot.
 	            System.out.println(line);
 	            
-	            if (line.startsWith("PING ")) {
+	            if (line.startsWith("PING ")) { //$NON-NLS-1$
 	                // We must respond to PINGs to avoid being disconnected.
-	            	logger.trace("Bot was pinged by server: " + line);
-	                writer.write("PONG " + line.substring(5) + "\r\n");
+	            	logger.trace("Bot was pinged by server: " + line); //$NON-NLS-1$
+	                writer.write("PONG " + line.substring(5) + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$
 	                writer.flush( );
 	            }
 	            
 	            
-	            for (String i : line.split("\\s+"))
+	            for (String i : line.split("\\s+")) //$NON-NLS-1$
 	            {
-	            	if (i.startsWith(":http://www.youtube.com/watch?v=") || i.startsWith(":https://www.youtube.com/watch?v="))
+	            	if (i.startsWith(":http://www.youtube.com/watch?v=") || i.startsWith(":https://www.youtube.com/watch?v=")) //$NON-NLS-1$ //$NON-NLS-2$
 	            	{
-	            		logger.trace("Youtube link posted.");
+	            		logger.trace("Youtube link posted."); //$NON-NLS-1$
 	            		YoutubeLink y = new YoutubeLink(i.substring(1));
-	            		writer.write("PRIVMSG " + channel + " :" + "7(Youtube7) " + y.getTitle() + " " + y.getDuration() + "\r\n");
-	            		writer.write("PRIVMSG " + channel + " :" + "7(Youtube7) " + y.getDescription() + "\r\n");
+	            		writer.write("PRIVMSG " + channel + " :" + "7(Youtube7) " + y.getTitle() + " " + y.getDuration() + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+	            		writer.write("PRIVMSG " + channel + " :" + "7(Youtube7) " + y.getDescription() + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	            		writer.flush();
 	            	}
-	            	else if (i.startsWith(":!online") && line.split("\\s+")[3].equals(":!online"))
+	            	else if (i.startsWith(":!online") && line.split("\\s+")[3].equals(":!online")) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	            	{
-	            		logger.trace("!online command posted");
+	            		logger.trace("!online command posted"); //$NON-NLS-1$
 	                    // Connect to CQ2
 	            		if (cq == null)
 	            			cq = new CQ2(cq2User, cq2Pass);
-	            		String user = line.split("\\s+")[4];
+	            		String user = line.split("\\s+")[4]; //$NON-NLS-1$
 	            		String x = cq.isOnline(user);
 	            		if (null == x)
 	            		{
-	            			writer.write("PRIVMSG " + channel + " :" + user + "? I'm having issues finding that mage right now. I might be having some sort of connection issue. Try again in a few seconds..."+ "\r\n");
-	            			logger.trace("PRIVMSG " + channel + " :" + user + "? I'm having issues finding that mage right now. I might be having some sort of connection issue. Try again in a few seconds..."+ "\r\n");
+	            			writer.write("PRIVMSG " + channel + " :" + user + "? I'm having issues finding that mage right now. I might be having some sort of connection issue. Try again in a few seconds..."+ "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+	            			logger.trace("PRIVMSG " + channel + " :" + user + "? I'm having issues finding that mage right now. I might be having some sort of connection issue. Try again in a few seconds..."+ "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
 	            			writer.flush();
 	            			cq = null;
 	            		}
 	            		else
 	            		{
-	            			writer.write("PRIVMSG " + channel + " :" + x + "\r\n");
-	            			logger.trace("PRIVMSG " + channel + " :" + x + "\r\n");
+	            			writer.write("PRIVMSG " + channel + " :" + x + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	            			logger.trace("PRIVMSG " + channel + " :" + x + "\r\n"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	            			writer.flush();
 	            		}
 	            	}
-	            	else if (i.startsWith(":!rescheck") && line.split("\\s+")[3].equals(":!rescheck"))
+	            	else if (i.startsWith(":!rescheck") && line.split("\\s+")[3].equals(":!rescheck")) //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 	            	{
-	            		logger.trace("!rescheck command posted");
+	            		logger.trace("!rescheck command posted"); //$NON-NLS-1$
 	            		// Connect to CQ2
 	            		if (cq == null)
 	            			cq = new CQ2(cq2User, cq2Pass);
-	            		String user = line.split("\\s+")[4];
+	            		String user = line.split("\\s+")[4]; //$NON-NLS-1$
 	            		cq.resCheck(user);
 	            	}
 	            }
--- src/client/YoutubeLink.java
+++ src/client/YoutubeLink.java
@@ -5,55 +5,49 @@ import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-
-
 public class YoutubeLink {
 	private String title = "";
 	private String duration = "";
-	private int durationMin = 0, durationSec = 0; 
+	private int durationMin = 0, durationSec = 0;
 	private String description = "";
-	
-	YoutubeLink(String url)
-	{
+
+	YoutubeLink(String url) {
 		try {
-			
+
 			Document doc = Jsoup.connect(url).get();
 			title = doc.title();
 			duration = doc.select("meta[itemprop=duration]").attr("content");
 			duration = duration.replace("PT", "");
-			duration = duration.replace("S","");
-			
+			duration = duration.replace("S", "");
+
 			durationMin = Integer.parseInt(duration.split("M")[0]);
-			durationSec = Integer.parseInt(duration.split("M")[1]);	
-			
+			durationSec = Integer.parseInt(duration.split("M")[1]);
+
 			description = doc.select("meta[name=description]").attr("content");
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 
-	YoutubeLink()
-	{
-		
+	YoutubeLink() {
+
 	}
-	
-	public String getTitle()
-	{
+
+	public String getTitle() {
 		return title;
 	}
-	
-	public String getDuration()
-	{
-		if(durationMin < 60)
+
+	public String getDuration() {
+		if (durationMin < 60)
 			return String.format("(%d:%02d)", durationMin, durationSec);
 		else
-			return String.format("(%d:%d:%02d)", durationMin/60, durationMin%60, durationSec);
+			return String.format("(%d:%d:%02d)", durationMin / 60,
+					durationMin % 60, durationSec);
 	}
-	
-	public String getDescription()
-	{
+
+	public String getDescription() {
 		return description;
 	}
 }
