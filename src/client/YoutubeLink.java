af56c0d331638ec649572cea69fa794bce814521
Hebron George <hebrontgeorge@gmail.com>
2015-06-10 00:12:52 -0400

    Updated YoutubeLink to now extend Link

--- src/client/YoutubeLink.java
+++ src/client/YoutubeLink.java
@@ -1,43 +1,26 @@
 package client;
 
-import java.io.IOException;
+import org.jsoup.nodes.Element;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 
-public class YoutubeLink {
-	private String title = "";
+
+public class YoutubeLink extends Link {
 	private String duration = "";
 	private int durationMin = 0, durationSec = 0;
-	private String description = "";
+	private String description = "", likes = "", views = "";
 
 	YoutubeLink(String url) {
-		try {
+		super(url);
+		duration = doc.select("meta[itemprop=duration]").attr("content");
+		duration = duration.replace("PT", "");
+		duration = duration.replace("S", "");
 
-			Document doc = Jsoup.connect(url).get();
-			title = doc.title();
-			duration = doc.select("meta[itemprop=duration]").attr("content");
-			duration = duration.replace("PT", "");
-			duration = duration.replace("S", "");
+		durationMin = Integer.parseInt(duration.split("M")[0]);
+		durationSec = Integer.parseInt(duration.split("M")[1]);
 
-			durationMin = Integer.parseInt(duration.split("M")[0]);
-			durationSec = Integer.parseInt(duration.split("M")[1]);
-
-			description = doc.select("meta[name=description]").attr("content");
-
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		description = doc.select("meta[name=description]").attr("content");
 	}
 
-	YoutubeLink() {
-
-	}
-
-	public String getTitle() {
-		return title;
-	}
 
 	public String getDuration() {
 		if (durationMin < 60)
@@ -49,5 +32,28 @@ public class YoutubeLink {
 
 	public String getDescription() {
 		return description;
+	}
+
+	@Override
+	public String summary() {
+		// TODO Auto-generated method stub
+		String formattedDuration = getDuration().replace('(', ' ');
+		formattedDuration = formattedDuration.replace(')', ' ');
+		formattedDuration = formattedDuration.trim();
+		
+		String formattedTitle = getTitle();
+		
+		// Remove the "- Youtube" at the end of title
+		String[] split = formattedTitle.split("-");
+		formattedTitle = split[0];
+		for (int i = 1; i < split.length-1; i++) {
+			formattedTitle += " - " + split[i] ;
+		}
+		// Get number of views and likes
+		views = doc.select(".watch-view-count").get(0).html();
+		Element e = doc.select("button[title=I like this").get(0);
+		likes = e.getAllElements().get(1).html();
+		
+		return "7(Youtube 7| " + formattedDuration + " 7| " + views + " views 7| " + likes + " likes" +"7) " + formattedTitle;
 	}
 }
